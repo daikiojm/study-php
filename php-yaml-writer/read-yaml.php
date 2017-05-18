fe774afa72eb21fe60e0c25784eb018af096cd6d
= <=>
2017-05-18 23:26:37 +0900

    add all

--- php-yaml-writer/read-yaml.php
+++ php-yaml-writer/read-yaml.php
@@ -3,6 +3,11 @@
 require __DIR__ . '/vendor/autoload.php';
 
 use Symfony\Component\Yaml\Yaml;
+use Symfony\Component\Yaml\Exception\ParseException;
 
-$value = Yaml::parse(file_get_contents('input.yaml'));
+try {
+    $value = Yaml::parse(file_get_contents('input.yaml'));
+} catch (ParseException $e) {
+    printf("Unable to parse the YAML string: %s", $e->getMessage());
+}
 var_dump($value);
